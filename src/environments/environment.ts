fae94d56f12f693daea958e542f205f6d5d82aef
Julian Rodriguez G <julian.rodriguezg@udea.edu.co>
2025-11-20 18:46:28 -0500

    Update baseUrl in environment configuration for development deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://k8s-cominow-clientga-487d3ec347-410239c1ef728475.elb.us-east-2.amazonaws.com/api',
+  baseUrl: 'https://z2j7v360-3000.use.devtunnels.ms/api',
 };
